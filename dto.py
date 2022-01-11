6272583fce812d1e26f556c228f92e7e3519d101
liorashkenazy <la26597@gmail.com>
2022-01-11 16:21:54 +0200

    Implementation of all DTO objects

---
+++ dto.py
@@ -0,0 +1,24 @@
+# DTO represents hats table
+class Hat:
+    def __init__(self, id, topping, supplier, quantity):
+        self.id = id
+        self.topping = topping
+        self.supplier = supplier
+        self.quantity = quantity
+
+
+# DTO represents suppliers table
+class Supplier:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+# DTO represents orders table
+class Order:
+    def __init__(self, id, location, hat):
+        self.id = id
+        self.location = location
+        self.hat = hat
+
+
